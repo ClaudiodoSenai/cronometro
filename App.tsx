9c8f9fc57a871748c865d3adca768ea67e1b7817
claudiorobertojunior5@gmail.com <claudiorobertojunior5@gmail.com>
2024-03-05 15:04:50 -0300

    05/03

--- App.tsx
+++ App.tsx
@@ -1,118 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState, useEffect } from "react";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Image,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  Text, TouchableOpacity, View
+} from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App(): React.JSX.Element {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+
+    <View style = {styles.container}>
+      <Image source={require('./src/assets/images/crono.png')} />
+
+      <Text style={styles.timer}>00:00:00</Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style = {styles.btnTexto}>Iniciar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn}>
+          <Text style = {styles.btnTexto}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style ={styles.areaTempo}>
+      <Text style ={styles.textoCorrida}>
+        Ultimo Tempo: 00:00:00
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+      </View>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1 ,
+    alignItems:'center',
+    justifyContent: 'center',
+    backgroundColor:'#00aeef'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  areaTempo: {
+    marginTop:40
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  textoCorrida: {
+    fontSize:20,
+    color:'#FFF',
+    fontStyle:'italic'
   },
-  highlight: {
-    fontWeight: '700',
+  timer: {
+    marginTop: -160,
+    fontSize: 45,
+    fontWeight:'bold',
+    color:'#FFF'
   },
-});
+  btn: {
 
+  },
+  btnTexto: {
+
+  },
+  btnArea: {
+
+  }
+
+});
 export default App;
